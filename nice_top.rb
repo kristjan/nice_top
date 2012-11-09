6cb4b7882bb0d6f15a7ac2733354abe2725372c5
Kristján Pétursson <kristjan@gmail.com>
2012-11-08 20:43:17 -0800

    Refactor `set_deaktop`
    Now it can be used from anywhere

--- nice_top.rb
+++ nice_top.rb
@@ -7,6 +7,19 @@ TUMBLR_API_BASE = "https://api.tumblr.com/v2"
 BLOG_NAME = "fuckyeahprettyplaces.tumblr.com"
 POSTS_PATH = "/blog/#{BLOG_NAME}/posts"
 
+SET_DESKTOP = <<-SCRIPT
+  on run argv
+    tell application "Finder"
+      set desktop picture to POSIX file (item 1 of argv)
+    end tell
+  end run
+SCRIPT
+
+def set_desktop(image_path)
+  args = SET_DESKTOP.split("\n").map{|l| "-e '#{l}'"}.join(' ')
+  `osascript #{args} #{image_path}`
+end
+
 photos = HTTParty.get(TUMBLR_API_BASE + POSTS_PATH, {
   :query => {
     :api_key => ENV["TUMBLR_API_KEY"],
@@ -18,17 +31,8 @@ photo_url = photos.first["original_size"]["url"]
 name = photo_url.split('/').last
 path = "/tmp/#{name}"
 
-SET_DESKTOP = <<-SCRIPT
-  on run argv
-    tell application "Finder"
-      set desktop picture to POSIX file (item 1 of argv)
-    end tell
-  end run
-SCRIPT
-
 if !File.exists?(path)
   `curl -s #{photo_url} > #{path}`
-  args = SET_DESKTOP.split("\n").map{|l| "-e '#{l}'"}.join(' ')
-  `osascript #{args} #{path}`
+  set_desktop(path)
 end
 
